4fa5480bc7364899f9c7355234bb9f1ae4a863d1
Mweh <fahmianton@gmail.com>
2023-09-05 20:00:42 +0700

    v1.3 Animation challenge done

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 import ColorKit
 
+struct ProminentTitle: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.weight(.heavy))
+            .foregroundStyle(.white)
+            .shadow(radius: 2)
+    }
+}
+
+extension View{
+    func prominentTitle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
+
 struct ContentView: View {
     @State private var isAlertScore = false
     @State private var alertMessage = ""
@@ -18,19 +33,34 @@ struct ContentView: View {
     @State private var isFinish = false
     @State private var tapCount = 0
     
+    @State private var animationAmount = 0.0
+    @State private var opacityAmount = 1.0
+    @State private var scaleAmount = 1.0
+    
+    struct FlagImage: View {
+        let flag: String
+        var body: some View{
+            Image(flag)
+                .renderingMode(.original)
+                .clipShape(Capsule())
+                .shadow(radius: 5)
+                .opacity(1)
+        }
+    }
+    
     var body: some View {
         ZStack{
             RadialGradient(stops:[
-//                .init(color: Color(hex: "3E19898"), location: 0.5),
                 .init(color: Color(uiColor: UIColor(hex: "393E46") ?? .black), location: 0.4),
                 .init(color: Color(uiColor: UIColor(hex: "00ADB5") ?? .black), location:0.4)
             ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
             VStack{
                 Text("Guess the flag")
-                    .font(.largeTitle.weight(.heavy))
-                    .foregroundStyle(.white)
-                    .shadow(radius: 2)
+                    .prominentTitle() // custom modifier
+                Spacer()
+                Spacer()
+
                 VStack(spacing: 30){
                     VStack{
                         Text("Tap the flag of")
@@ -39,17 +69,24 @@ struct ContentView: View {
                             .font(.largeTitle.weight(.semibold))
                     }
                     ForEach(0..<3){ number in
-                        Button{
+                        Button(){
                             flagTapped(number)
                             userAnswer = number
-                            print("Flag was tapped")
-                            print("Flag: \(number)")
+                            withAnimation(.easeInOut(duration: 1)){
+                                scaleAmount = 0
+                                opacityAmount = 0
+                                animationAmount += 360
+                            }
+                            print("userAnswer was \(userAnswer)")
+                            print("number: \(number)")
+                            print("\(opacityAmount)")
                         } label: {
-                            Image(flag[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(flag: flag[number]) // custom view with specific modifiers
                         }
+                        .rotation3DEffect(.degrees(number == userAnswer ? animationAmount : 0), axis: (x: 0, y: 1, z: 0))
+                        .opacity(number != userAnswer ? opacityAmount : 1)
+                        .scaleEffect(number != userAnswer ? scaleAmount : 1 )
+                        
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -57,9 +94,8 @@ struct ContentView: View {
                 .background(.ultraThinMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 Spacer()
-                Spacer()
                 Text("Score: \(score)")
-                    .font(.title.bold())
+                    .font(.largeTitle.bold())
                     .foregroundStyle(.white)
                 Spacer()
             }
@@ -91,9 +127,14 @@ struct ContentView: View {
         }
         isAlertScore = true
     }
+    
     func askQustion(){
-        flag.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        withAnimation{
+            scaleAmount = 1
+            opacityAmount = 1
+            flag.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+        }
     }
     func reset() {
         score = 0
@@ -124,4 +165,14 @@ struct ContentView_Previews: PreviewProvider {
 //
 //        self.init(red: red, green: green, blue: blue)
 //    }
+//}
+
+//let agents = ["Cyril", "Lana", "Pam", "Sterling"]
+//
+//ForEach(0 ..< 100) { number in
+//    Text("Row \(number)")
+//}
+//
+//ForEach(agents, id: \.self) {
+//    Text("Agents: \($0)")
 //}
